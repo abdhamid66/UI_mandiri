d179ed1170ed63bed3716c6bdf43b5414057be62
abd.hamid <hamnsxx@gmail.com>
2026-03-19 13:40:14 +0700

    tampilan awal bank mandiri

--- lib/main.dart
+++ lib/main.dart
@@ -7,116 +7,143 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
+@override
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomePage extends StatelessWidget{
+  const HomePage({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+@override
+Widget build(BuildContext context){
+  return Scaffold(
+    backgroundColor: Colors.blue[900],
+    
+    // appBar: AppBar
+    appBar: AppBar(
+      backgroundColor: Colors.blue[900],
+      elevation: 0,
+      title: const Text(" by Mandiri"),
+      actions: const [
+        Padding(
+          padding: EdgeInsets.all(8.0),
+        )
+      ],
+    ),
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    // body
+    body: ListView(
+      children: [
+        // saldo
+        Container(
+          margin: const EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.blue[700],
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text("saldo Anda", style: TextStyle(color: Colors.white70),
+              ),
+              SizedBox(height: 8),
+              Text("Rp 20.000.000", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),),
+            ],
+          ),
+        ),
+        // menu
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 16),
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Column(children: [
+            // row 1
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                menuItem(Icons.send, "Transfer"),
+                menuItem(Icons.payment, "bayar"),
+                menuItem(Icons.phone_android, "Pulsa"),
+                menuItem(Icons.account_balance, "Top Up"),
+              ],
+            ),
+            const SizedBox(height: 20),
+            // row 2
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                menuItem(Icons.account_balance_wallet, "Tagihan"),
+                menuItem(Icons.qr_code_scanner, "Scan QR"),
+                menuItem(Icons.receipt_long, "Riwayat"),
+                menuItem(Icons.settings, "Lainnya"),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      const SizedBox(height: 20),
+
+      // list transaksi
+      Container(
+        margin: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+                Text(
+                  "Transaksi Terakhir",
+                  style: TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+              ],
+        ),
+        ),
+      // LISTVIEW ITEM
+          ListTile(
+            leading: Icon(Icons.arrow_upward, color: Colors.red),
+            title: Text("Transfer ke Budi"),
+            subtitle: Text("Kemarin"),
+            trailing: Text("- Rp 200.000"),
+          ),
+
+          ListTile(
+            leading: Icon(Icons.arrow_downward, color: Colors.green),
+            title: Text("Gaji Masuk"),
+            subtitle: Text("2 hari lalu"),
+            trailing: Text("+ Rp 5.000.000"),
+          ),
+
+          ListTile(
+            leading: Icon(Icons.payment, color: Colors.orange),
+            title: Text("Bayar Listrik"),
+            subtitle: Text("3 hari lalu"),
+            trailing: Text("- Rp 150.000"),
+          ),
+        ],
       ),
+    );
+  }
+  // widget menu item
+  static Widget menuItem(IconData icon, String title) {
+    return Column(
+      children: [
+        CircleAvatar(
+          backgroundColor: Colors.blue[100],
+          child: Icon(icon, color: Colors.blue[900]),
+        ),
+        const SizedBox(height: 8),
+        Text(title, style: const TextStyle(fontSize: 12)),
+      ],
     );
   }
 }
